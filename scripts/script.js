da6c987f07926ed1dbc7544fc20a60a421825897
billyzhu97 <32204701+billyzhu97@users.noreply.github.com>
2017-12-30 11:44:00 -0500

    Got Rid of Parallax
    Parallax was not working well on Firefox

--- scripts/script.js
+++ scripts/script.js
@@ -32,13 +32,4 @@ $(document).ready(function(){
     });
   });
 
-  var jumboHeight = $('.jumbotron').outerHeight();
-  function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-  }
-
-  $(window).scroll(function(e){
-    parallax();
-  });
 })
